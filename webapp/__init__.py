605aa966a818900a4656fbe5e838a06dcee44881
Vasilii Polikarpov <polik94pvn@gmail.com>
2025-01-25 18:56:35 +0100

    Add database initialization command

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,8 +10,14 @@ def create_app():
   app = Flask(__name__)
   app.config.from_prefixed_env()
 
+  # Initialize database
   database.db.init_app(app)
 
+  # Register click commands
+  app.cli.add_command(database.init_db_command)
+
+  # Register blueprints
   app.register_blueprint(tracker.bp)
   app.register_blueprint(foods.bp)
+
   return app
--- webapp/database.py
+++ webapp/database.py
@@ -1,3 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import current_app
+import click
 
 db = SQLAlchemy()
+
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
+
+
+@click.command("init-db")
+def init_db_command():
+    click.echo("Initializing the database...")
+    click.echo("Database uri: " +
+               current_app.config["SQLALCHEMY_DATABASE_URI"])
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")
